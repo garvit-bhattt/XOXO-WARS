f37cabf67835647c9db1fb20f9d6ca969f85e764
GARVIT <garvitbhattpc@gmail.com>
2025-05-12 12:20:30 +0530

    Update script.js
    new game not showing after draw issue fixed

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ boxes.forEach((box) => {
     const gameDraw = () => {
   msg.innerText = `Game was a Draw`;
   msgcontainer.classList.remove("hide");
+  newgamebtn.classList.remove("hide");
   disablebox();
 };
 
